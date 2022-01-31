234d425d07f5228864dbfd67628ea54e0a6cc1ba
talaveraedson <st2347@utr.edu.mx>
2022-01-31 14:37:29 -0600

    Tercer Update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Message{
+    public void Print(){
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Principal{
+    static void Main (){
+        Message mensaje = new Message();
+        mensaje.Print();
+    }
+}
+
+//f1+beauty
+/* alt-shift-a */
